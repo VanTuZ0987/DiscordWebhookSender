4ee3f4dbd8fc163ac85c21d8a1cce9ae2b69030f
VanTuZ0987 <sasha.kry58@gmail.com>
2026-03-07 16:06:53 +0300

    Add project files

---
+++ Discord.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace DiscordWebhookSender
+{
+    static class Discord
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+        private static string? Token;
+        private static string? BotName;
+        private static string? AvatarUrl;
+        public static void SetToken(string token)
+        {
+            Token = token;
+        }
+
+        public static void SetBotName(string name)
+        {
+            BotName = name;
+        }
+
+        public static void SetBotAvatarUrl(string url)
+        {
+            AvatarUrl = url;
+        }
+
+        public static async Task SendMessage(string msg)
+        {
+            if (string.IsNullOrEmpty(Token))
+            {
+                throw new InvalidOperationException("You forgot initialize the token");
+            }
+
+            var message = new
+            {
+                content = msg,
+                username = BotName,
+                avatar_url = AvatarUrl
+
+            };
+
+            string json = System.Text.Json.JsonSerializer.Serialize(message);
+            try
+            {
+
+                using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, Token)
+                {
+                    Content = new StringContent(json, Encoding.UTF8, "application/json")
+                };
+                var response = await httpClient.SendAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static async Task SendRepeatedMessages(string msg, int amount, int delaySecond = 1, bool marker = false)
+        {
+            for (int i = 1; i <= amount; i++)
+            {
+                string message = marker ? $"{msg} [{i}]" : msg;
+                await SendMessage(message);
+                await Task.Delay(delaySecond * 1000);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DiscordWebhookSender
+{
+    internal class Program
+    {
+        //static async Task Main(string[] args)
+        //{
+        //    //Discord.SetToken("Token");
+        //    //Discord.SetBotName("Albert"); optional
+        //    //Discord.SetBotAvatarUrl("https://example.com/image"); optional
+        //    //await Discord.SendMessage("Hello");
+        //    //await Discord.SendRepeatedMessages("Hello", 2, 1);
+        //}
+    }
+    
+}
